bfb2748961a587346e8eda5d5510a75c4855dd31
axander <axander.wilson@gmail.com>
2020-11-18 18:33:30 -0500

    search and login authentication added

---
+++ inventory_api/permissions.py
@@ -0,0 +1,30 @@
+from rest_framework import permissions
+
+class UpdateOwnProfile(permissions.BasePermission):
+    """Allow users to edit their own profile"""
+    def has_object_permission(self, request, view, obj):
+        """Check user is trying to edit their own profile"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow users to update their own status"""
+    def  has_object_permission(self, request, view, obj):
+        """Check user is trying to update their own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user_profile.id == request.user.id
+
+
+## TODO: create permission that allows authenticated users to get data read_only
+## BUT restricts post and update methods to user where is_staff == True
+"""
+    Permissions class only for manipulating products, regions, brands, and orders
+
+            if request.method in permissions.SAFE_METHODS:
+                return True
+            return request.user.is_staff == True
+
+"""
---
+++ inventory_api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from inventory_api import views
+
+
+
+router = DefaultRouter()
+router.register('profile',views.UserProfileViewSet)
+
+urlpatterns = [
+    path('login/',views.UserLoginApiView.as_view()),
+    path('',include(router.urls)),
+
+]
